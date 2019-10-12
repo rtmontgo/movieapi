b66f5dde5c2c6fa06ab1d520bcbdd855eab312c1
Taylor <r.taylor.montgomery@gmail.com>
2019-10-11 21:48:07 -0500

    updates

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.post('/users',
     var errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.json(422).json({ errors: errors.array()
+      return res.status(422).json({ errors: errors.array()
       });
     }
 
