2d2551b0f4f9f2c2b3411d5c84fc0f8e8277c5cc
Taylor <r.taylor.montgomery@gmail.com>
2019-10-13 15:57:51 -0500

    resubmit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const  app = express();
 const  morgan = require('morgan');
 const bodyParser = require('body-parser');
+const uuid = require('mongoose');
 const mongoose = require('mongoose');
 const Models = require('./models.js');
 
--- models.js
+++ models.js
@@ -1,36 +1,39 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 var movieSchema = mongoose.Schema({
-  Title : {type: String, required: true},
-  Description : {type: String, required: true},
-  Genre : {
-    Name : String,
-    Description : String
+  Title:{type: String, required: true},
+  Description:{type: String, required: true},
+  Genre: {
+    Name: String,
+    Description: String
   },
   Director : {
-    Name : String,
-    Bio : String
+    Name: String,
+    Bio: String,
+    BirthYear: Number
   },
-  ImagePath : String,
-  Featured : Boolean
+  ImageUrl:String,
+  Featured: Boolean
 });
-
-const bcrypt = require('bcrypt');
 
 var userSchema = mongoose.Schema({
   Username : {type: String, required: true},
   Password : {type: String, required: true},
   Email : {type: String, required: true},
   Birthday : Date,
-  Favorites : [{ type : mongoose.Schema.Types.ObjectId, ref : 'Movie'}]
+  Favourites : [{ type : mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
+userSchema.statics.hashPassword = function(password) {
+  return bcrypt.hashSync(password, 10);
+};
 userSchema.methods.validatePassword = function(password) {
-  return bcrypt.compareSync(password, this.Password); };
+  return bcrypt.compareSync(password, this.Password);
+};
 
-
-var Movie = mongoose.model('Movie', movieSchema);
-var User = mongoose.model('User', userSchema);
+var Movie = mongoose.model('Movie',movieSchema);
+var User = mongoose.model('User',userSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
