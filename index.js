8e59343e882c405344876c2a4ed6a119f17f7525
Taylor <r.taylor.montgomery@gmail.com>
2019-10-14 11:28:39 -0500

    redo

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const cors = require('cors')
-const validator = require('express-validator');
-const passport = require('passport');
-const Models = require('./models.js');
-
-
-require('./passport');
-
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Models = require('./models.js');
 
 const Movies = Models.Movie;
 const Users = Models.User;
+const passport = require('passport');
+const cors = require('cors');
+const { check, validationResult } = require('express-validator');
+require('./passport');
+
+mongoose.set('useFindAndModify', false);
+//mongoose.connect('mongodb://localhost:27017/HoHdb', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://rtmontgo:Zombie3%21@tmont-3jagp.mongodb.net/HoHdb?retryWrites=true&w=majority', { useNewUrlParser: true });
 
 //Middleware functions
+app.use(express.static('public'));
+app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('public'));
-
-
-app.use(morgan('common'));
 
 const auth = require('./auth')(app);
 
-mongoose.connect('mongodb+srv://rtmontgo:Zombie3%21@tmont-3jagp.mongodb.net/HoHdb?retryWrites=true&w=majority', { useNewUrlParser: true });
 
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something got slashed!');
+});
 
 //GET requests
 app.get('/', function (req, res) {
@@ -103,44 +105,44 @@ app.get('/users', passport.authenticate('jwt', { session: false }), function (re
 //   Birthday : Date
 // }
 
-app.post('/users', (req, res) => {
-  // Validation logic here for request
-  req.checkBody('Username', 'Username is required').isLength({ min: 5 }),
-    req.checkBody('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
-    req.checkBody('Password', 'Password is required').notEmpty(),
-    req.checkBody('Email', 'Email does not appear to be valid').isEmail()
+app.post('/users',
+  [check('Username', 'Username is required').isLength({ min: 5 }),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()], (req, res) => {
+    //check validation object for errors
+    var errors = validationResult(req);
 
-  //check validation object for errors
-  var errors = req.validationErrors();
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array()
+      });
+    }
 
-  if (errors) {
-    return res.status(422).json({ errors: errors });
-  }
-
-  var hashedPassword = Users.hashPassword(req.body.Password);
-  Users.findOne({ Username: req.body.Username })
-    .then(function (user) {
-      if (user) {
-        return res.status(400).send(req.body.Username + "already exists");
-      } else {
-        Users
-          .create({
-            Username: req.body.Username,
-            Password: hashedPassword,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
-          })
-          .then(function (user) { res.status(201).json(user) })
-          .catch(function (error) {
-            console.error(error);
-            res.status(500).send("Error: " + error);
-          })
-      }
-    }).catch(function (error) {
-      console.error(error);
-      res.status(500).send("Error: " + error);
-    });
-});
+    var hashedPassword = Users.hashPassword(req.body.Password);
+    Users.findOne({ Username: req.body.Username })
+      .then(function (user) {
+        if (user) {
+          return res.status(400).send(req.body.Username + "already exists");
+        } else {
+          Users
+            .create({
+              Username: req.body.Username,
+              Password: hashedPassword,
+              Email: req.body.Email,
+              Birthday: req.body.Birthday
+            })
+            .then(function (user) { res.status(201).json(user) })
+            .catch(function (error) {
+              console.error(error);
+              res.status(500).send("Error: " + error);
+            })
+        }
+      }).catch(function (error) {
+        console.error(error);
+        res.status(500).send("Error: " + error);
+      });
+  });
 
 //Delete a user profille
 app.delete('/users/:Username', passport.authenticate('jwt', { session: false }), function (req, res) {
@@ -158,39 +160,43 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
     });
 });
 
-// update user info
-app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
-  req.checkBody('Username', 'Username is required').notEmpty();
-  req.checkBody('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric();
-  req.checkBody('Password', 'Password is required').notEmpty();
-  req.checkBody('Email', 'Email is required').notEmpty();
-  req.checkBody('Email', 'Email does not appear to be valid').isEmail();
+//Update user profile
+app.put('/users/:Username', passport.authenticate('jwt', { session: false }),
+  [check('Username', 'Username is required').isLength({ min: 5 }),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()],
+  (req, res) => {
 
-  // check the validation object for errors
-  const errors = req.validationErrors();
+    var errors = validationResult(req);
 
-  if (errors) {
-    return res.status(422).json({ errors: errors });
-  }
-  const hashedPassword = Users.hashPassword(req.body.Password);
-  Users.update({ Username: req.params.username }, {
-    $set: {
-      Username: req.body.Username,
-      Password: hashedPassword,
-      Email: req.body.Email,
-      Birthday: req.body.Birthday,
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array()
+      });
     }
-  },
-    { new: true }, // this line makes sure that the updated document is returned
-    (error, updatedUser) => {
-      if (error) {
-        console.error(error);
-        res.status(500).send('Error: ' + error);
-      } else {
-        res.json(updatedUser)
+
+    var hashedPassword = Users.hashPassword(req.body.Password);
+
+    Users.findOneAndUpdate({ Username: req.params.Username }, {
+      $set:
+      {
+        Username: req.body.Username,
+        Password: hashedPassword,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
       }
-    })
-});
+    },
+      { new: true }, //Makes sure updated doc gets returned
+      function (err, updatedUser) {
+        if (err) {
+          console.error(err);
+          res.status(500).send("Error: " + err);
+        } else {
+          res.json(updatedUser)
+        }
+      })
+  });
 
 //Add a movie to the favorites list
 app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), function (req, res) {
@@ -222,11 +228,6 @@ app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { se
         res.json(updatedUser)
       }
     });
-});
-
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something got slashed!');
 });
 
 
