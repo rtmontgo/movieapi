4df9b41fdd73cbc6d83b23459a13b7f57d5f657a
Taylor <r.taylor.montgomery@gmail.com>
2019-11-04 20:01:58 -0600

    change port

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { se
 
 
 //listen for requests
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 1234;
 app.listen(port, "0.0.0.0", function () {
-  console.log("Listening on Port 3000");
+  console.log("Listening on Port 1234");
 });
